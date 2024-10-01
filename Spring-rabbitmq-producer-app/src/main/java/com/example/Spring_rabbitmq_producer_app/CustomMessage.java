f0d55daa1fd3e5581f2a358b8ccb8d5f764bca30
manoejKr <287642@ust.com>
2024-10-01 12:15:06 +0530

    first commit

---
+++ Spring-rabbitmq-producer-app/src/main/java/com/example/Spring_rabbitmq_producer_app/CustomMessage.java
@@ -0,0 +1,41 @@
+package com.example.Spring_rabbitmq_producer_app;
+
+import java.util.Date;
+
+
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
+
+ 
+public class CustomMessage {
+private String messageId;
+public CustomMessage(String messageId, String message, Date messageDate) {
+    this.messageId = messageId;
+    this.message = message;
+    this.messageDate = messageDate;
+}
+private String message;
+private Date messageDate;
+
+public String getMessageId() {
+    return messageId;
+}
+public String getMessage() {
+    return message;
+}
+public Date getMessageDate() {
+    return messageDate;
+}
+public void setMessageId(String messageId) {
+    this.messageId = messageId;
+}
+public void setMessage(String message) {
+    this.message = message;
+}
+public void setMessageDate(Date date) {
+    this.messageDate = date;
+}
+
+
+}
---
+++ Spring-rabbitmq-producer-app/src/test/java/com/example/Spring_rabbitmq_producer_app/SpringRabbitmqProducerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_rabbitmq_producer_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitmqProducerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
